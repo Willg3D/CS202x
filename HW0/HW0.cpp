0c24c87d2fa8a947d61ba68062422ca962ca0bb4
William Glass <willg.glass94@gmail.com>
2021-01-22 18:44:11 -0900

    Added couts to show functions working.

--- HW0/HW0.cpp
+++ HW0/HW0.cpp
@@ -4,20 +4,22 @@
 
 double cpp_ftoc(const char* str) {
 	std::string temp(str);
-	temp.erase(0, 19);
+	temp.erase(0, 19); // removes extra everything but num
 	double f = std::stod(temp);
 	return ((f - 32.0) * (5.0 / 9.0));
 }
 
 double cpp_ctof(const char* str) {
 	double c = strtod(str, nullptr);
-	return ((c * 1.8) + 32);
+	return ((c * 1.8) + 32.0);
 }
 
 int main(int argc, char* str) {
-	std::string test = "temperature --ftoc -40";
-	char* c = const_cast<char*>(test.c_str());
-	//cpp_ftoc(c);
-	std::cout << cpp_ftoc(c);
+	std::string cInput = "temperature --ftoc 32";
+	char* c = const_cast<char*>(cInput.c_str());
+	std::cout << "32f to c: "<<cpp_ftoc(c) << std::endl;
+	std::string fInput = "temperature --ctof 0";
+	char* f = const_cast<char*>(fInput.c_str());
+	std::cout << "0c to f: " << cpp_ctof(f) << std::endl;
 	return 0;
 }
