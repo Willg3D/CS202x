8c1f6cf0c12f852481a5b5555dc138c4f98461d3
William Glass <willg.glass94@gmail.com>
2021-01-22 17:49:18 -0900

    Entered my quick previous attempt

--- HW0/HW0.cpp
+++ HW0/HW0.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
+#include <string> //std::stod
+#include <stdlib.h> //std::strtod
+
+double cpp_ftoc(const char* str) {
+	std::string temp(str);
+	temp.erase(0, 19);
+	double f = std::stod(temp);
+	return ((f - 32.0) * (5.0 / 9.0));
+}
+
+double cpp_ctof(const char* str) {
+
+	return 0;
+}
 
 int main() {
+	std::string str = "temperature --ftoc -40";
 
+	char* c = const_cast<char*>(str.c_str());
+	//cpp_ftoc(c);
+	std::cout << cpp_ftoc(c);
+	return 0;
 }
