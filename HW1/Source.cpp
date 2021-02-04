af2235c9f4d5611ab25c3f94510cf56020dd3484
William Glass <willg.glass94@gmail.com>
2021-02-03 22:40:28 -0900

    Made randomNum its own function.

--- HW1/Source.cpp
+++ HW1/Source.cpp
@@ -6,27 +6,37 @@ using std::endl;
 #include <vector>
 #include "StopWatch.h"
 
-void randomVect(std::vector<int> &rVector,int vectSize,int vectMin,int vectMax) {
-
+int randomNum(int min, int max) {
 	std::random_device rDevice;
 	std::mt19937 generator(rDevice());
-	std::uniform_int_distribution<int> distribution(vectMin, vectMax);
+	std::uniform_int_distribution<int> distribution(min, max);
 
-	for (size_t i = 0; i < vectSize; ++i) {
-		rVector.push_back(distribution(generator));
-	}
-
-
+	return distribution(generator);
 }
+
+void randomVect(std::vector<int> &rVector,int vectSize,int vectMin,int vectMax) {
+	rVector.clear(); // clears out vector
+	// Pushes random Values into vector.
+	for (size_t i = 0; i < vectSize; ++i) {
+		rVector.push_back(randomNum(vectMin,vectMax));
+	}
+}
+
+
 int main() {
 	//Stopwatch test;
 	std::vector<int> rVector; // random vector
-	randomVect(rVector, 1000, 0, 3000);
-
+	randomVect(rVector, 10, 0, 10);
+	
+	
+	for (size_t i = 0; i < 10; ++i) {
+		cout << rVector[i];
+	}
 
 
 	
 	//td::cout << x << std::endl;
 	//test.stop();
 	//std::cout << test.time_msec();
+	return 0;
 }
