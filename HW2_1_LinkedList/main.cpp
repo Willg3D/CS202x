596872fa8fffd7c8bc85d024c6694d563fba7917
William Glass <willg.glass94@gmail.com>
2021-02-11 19:39:41 -0900

    Created struct to hold values

--- HW2_1_LinkedList/main.cpp
+++ HW2_1_LinkedList/main.cpp
@@ -1,14 +1,25 @@
 #include <iostream>
 #include <list>
-#include <string>
-using std::string;
 
 
-struct Monster{
-	string Name
+struct Monster{ // Value Struct
+	int id; // used for order
+	char type; //Letter to indicate W-water R-rock G-grass
+	int hp;
+	int sta;
+	int att;
+	int def;
 };
 
 int main() {
+	std::list<Monster> values;
 
+	// Queue
+
+	// Stack
+
+	// Insert and Find
+
+	// Print the list out
 
 }
