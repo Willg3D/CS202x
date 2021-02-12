fd15b53e65864f02e29a6d510bf7c58e231b1553
William Glass <willg.glass94@gmail.com>
2021-02-12 03:24:34 -0900

    Added a bit more orginization

--- HW2_2_ MemoryLayout/main.cpp
+++ HW2_2_ MemoryLayout/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+using std::cout;
+using std::endl;
 
-int staticData = 2;
+int staticData;
 auto staticData_ptr = &staticData;
 
 // Changed print pointer fuction
@@ -12,9 +14,16 @@ void printPointer(int *i) {
 }
 
 int main() {
-	int stackData = 0;
+	int stackData;
 	auto stackData_ptr = &stackData;
 	auto freeStore_ptr = new int;
+
+	cout << "Static Data Pointer:" << endl;
+	printPointer(staticData_ptr);
+	cout << "Stack Data Pointer:" << endl;
+	printPointer(stackData_ptr);
+	cout << "Free Store Pointer:" << endl;
+	printPointer(freeStore_ptr);
 
 
 
