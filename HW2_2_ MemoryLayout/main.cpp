6d221db8c488b531fe25e0e092b75304688fa996
William Glass <willg.glass94@gmail.com>
2021-02-12 02:46:35 -0900

    Altered printPointer function

--- HW2_2_ MemoryLayout/main.cpp
+++ HW2_2_ MemoryLayout/main.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
 
-void printPointer(int i) {
-	int* iptr = &i;
+int staticData = 2;
+auto staticData_ptr = &staticData;
+
+// Changed print pointer fuction
+void printPointer(int *i) {
+	//int* iptr = &i;
 	// the unary * operator dereferences the pointer
-	std::cout << "i = " << *iptr << "\n";
-	std::cout << " & i = " << iptr << "\n";
+	std::cout << "i = " << *i << "\n";
+	std::cout << " & i = " << i << "\n";
 }
 
 int main() {
+	int stackData = 0;
+	auto stackData_ptr = &stackData;
+	auto freeStore_ptr = new int;
+
 
 
 	return 0;
