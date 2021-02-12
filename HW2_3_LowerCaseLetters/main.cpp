3b3b88d5e63f9686c78b866777daff5e0ae54171
William Glass <willg.glass94@gmail.com>
2021-02-12 05:06:50 -0900

    Added comments.

--- HW2_3_LowerCaseLetters/main.cpp
+++ HW2_3_LowerCaseLetters/main.cpp
@@ -1,6 +1,6 @@
 void to_lower(char* s) {
 	for (size_t i = 0; s[i] != '0'; i++){
-		//65 A to 90 Z and 32 defference to lowercase
+		//65 A to 90 Z and 32 defference to lowercase (ascii)
 		if (s[i] >= 65 && s[i] <= 90) {
 			s[i] = s[i] + 32;
 		}
@@ -8,15 +8,17 @@ void to_lower(char* s) {
 }
 
 int main() {
-	char text[] = "Hello World0!!";
+	// text array with terminating zero
+	char text[] = "Hello, World!0";
 	to_lower(text);
 
 	//check with out using iostream header
 	for (size_t i = 0; text[i] != '0'; i++) {
 		if (text[i] >= 65 && text[i] <= 90) {
-			// If check asscii values for upper case
+			// If check ascii values for upper case
 			// appears program will crash.
 			throw 1; // causes crash
+			//return 1; // could return 1 as an indicator if crashing is bad
 		}
 	}
 	return 0;
