f7938b842ddec0c0cf4cb82ff3490bc74a0f7724
William Glass <willg.glass94@gmail.com>
2021-02-12 04:34:18 -0900

    Made a quick test to validate 0 terminating

--- HW2_3_LowerCaseLetters/main.cpp
+++ HW2_3_LowerCaseLetters/main.cpp
@@ -1,10 +1,14 @@
+#include <iostream> //for testing
 
 void to_lower(char* s) {
-
+	for (size_t i = 0; s[i] != '0'; i++){
+		std::cout << s[i];
+	}
 }
 
 int main() {
-
+	char text[] = "Hello World0!!";
+	to_lower(text);
 
 	return 0;
 }
