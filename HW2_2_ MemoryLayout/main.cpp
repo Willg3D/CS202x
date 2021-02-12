b12bd0ce1baeef4e3ae112f924bbdec89bdc2e5c
William Glass <willg.glass94@gmail.com>
2021-02-12 04:04:31 -0900

    Quick test to determine address order

--- HW2_2_ MemoryLayout/main.cpp
+++ HW2_2_ MemoryLayout/main.cpp
@@ -25,7 +25,28 @@ int main() {
 	cout << "Free Store Pointer:" << endl;
 	printPointer(freeStore_ptr);
 
+	// Quick Test
+	if (stackData_ptr < staticData_ptr){
+		cout << "\nStack data pointer has a lower address than static data pointer" << endl;
+	}
+	if (stackData_ptr > staticData_ptr) {
+		cout << "\nStatic data pointer has a lower address than stack data pointer" << endl;
+	}
+	if (stackData_ptr < freeStore_ptr) {
+		cout << "\nStack data pointer has a lower address than free store pointer" << endl;
+	}
+	if (stackData_ptr > freeStore_ptr) {
+		cout << "\nFree store data pointer has a lower address than stack store pointer" << endl;
+	}
+	if (staticData_ptr < freeStore_ptr) {
+		cout << "\nStatic data pointer has a lower address than free store pointer" << endl;
+	}
+	if (staticData_ptr > freeStore_ptr) {
+		cout << "\nFree store pointer has a lower address than static data pointer" << endl;
+	}
+
+	// Hard coded answer based on test
+	cout << "\nOrder of Addresses from High to Low is Free Store, Stack Data, Static Data" << endl;
 
 
-	return 0;
 }
