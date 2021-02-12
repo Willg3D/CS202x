2d8d6f211944409d51958e2a0899fe316a001f92
William Glass <willg.glass94@gmail.com>
2021-02-11 19:49:27 -0900

    Rearranged code for convience

--- HW2_1_LinkedList/main.cpp
+++ HW2_1_LinkedList/main.cpp
@@ -1,25 +1,2 @@
-#include <iostream>
-#include <list>
-
-
-struct Monster{ // Value Struct
-	int id; // used for order
-	char type; //Letter to indicate W-water R-rock G-grass
-	int hp;
-	int sta;
-	int att;
-	int def;
-};
-
-int main() {
-	std::list<Monster> values;
-
-	// Queue
-
-	// Stack
-
-	// Insert and Find
-
-	// Print the list out
-
-}
+#define CATCH_CONFIG_MAIN // Tells catch to provide a main()
+#include "catch.hpp"
---
+++ HW2_1_LinkedList/tests.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <list>
+#include "catch.hpp"
+
+
+struct Monster { // Value Struct
+	int id; // used for order
+	char type; //Letter to indicate W-water R-rock G-grass
+	int hp;
+	int sta;
+	int att;
+	int def;
+};
+
+// Queue
+TEST_CASE("Push and Pop Test", "[Queue]") {
+
+
+}
+// Stack
+
+// Insert and Find
+
+// Print the list out
+
