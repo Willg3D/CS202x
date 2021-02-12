1a0f06897ebcb73d33ce6c23bad5a94a0843e82b
William Glass <willg.glass94@gmail.com>
2021-02-11 23:14:21 -0900

    Finished insert and find

--- HW2_1_LinkedList/tests.cpp
+++ HW2_1_LinkedList/tests.cpp
@@ -34,7 +34,7 @@ void enterMonValue(Monster& name, int id, char type, int hp, int sta, int att, i
 // Queue
 TEST_CASE("Push and Pop Test Queue", "[Queue]") {
 	std::list<Monster> test;
-	Monster v1,v2,v3,temp;
+	Monster v1, v2, v3;
 	enterMonValue(v1, 1, 'W', 1, 1, 1, 1);
 	enterMonValue(v2, 2, 'W', 2, 2, 2, 2);
 	enterMonValue(v3, 3, 'W', 3, 3, 3, 3);
@@ -54,7 +54,7 @@ TEST_CASE("Push and Pop Test Queue", "[Queue]") {
 // Stack
 TEST_CASE("Push and Pop Test Stack", "[Stack]") {
 	std::list<Monster> test;
-	Monster v1, v2, v3, temp;
+	Monster v1, v2, v3;
 	enterMonValue(v1, 1, 'W', 1, 1, 1, 1);
 	enterMonValue(v2, 2, 'W', 2, 2, 2, 2);
 	enterMonValue(v3, 3, 'W', 3, 3, 3, 3);
@@ -68,11 +68,43 @@ TEST_CASE("Push and Pop Test Stack", "[Stack]") {
 	REQUIRE(v2 == test.back()); //check if v2 in back after pop
 	test.pop_back();
 	REQUIRE(v1 == test.back()); //check if v3 in back after pop
-
 }
 // Insert and Find
 TEST_CASE("Insert and Find", "") {
+	std::list<Monster> test;
+	Monster v1, v2, v3, v4;
+	enterMonValue(v1, 1, 'W', 1, 1, 1, 1);
+	enterMonValue(v2, 2, 'W', 2, 2, 2, 2);
+	enterMonValue(v3, 3, 'W', 3, 3, 3, 3);
+	enterMonValue(v4, 4, 'R', 4, 4, 4, 4);
+	test.push_back(v1);
+	test.push_back(v2);
+	test.push_back(v4);
+	
+	// search for v2 
+	auto position = test.begin(); // position iter of test
+	for (size_t i = 0; i < test.size(); i++){
+		if (v2.id == (position->id)){ // if position id and v2 match then break
+			break;
+		}
+		std::advance(position, 1); // advances the iter
+	}
+	REQUIRE(v2 == *position); // verifies search position equals v2
 
+	// insert v3 in correct location
+	position = test.begin(); // position iter of test
+	for (size_t i = 0; i < test.size(); i++) {
+		if (v2.id == (position->id)) { // if position id and v2 match then break
+			std::advance(position, 1); // advances the iter
+			test.insert(position, v3);
+			break;
+		}
+		std::advance(position, 1); // advances the iter
+	}
+
+	position = test.begin(); // position iter of test
+	std::advance(position, v3.id - 1); // advances the iter to location of v3 id - 1
+	REQUIRE(v3 == *position); //verifies v3 is in expected position
 
 }
 // Print the list out
