6f2c957717ec90ac4db0e525b5da35305f37f4e5
William Glass <willg.glass94@gmail.com>
2021-02-12 00:56:33 -0900

    Added printPointer function from provided HW pdf

--- HW2_2_ MemoryLayout/main.cpp
+++ HW2_2_ MemoryLayout/main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 
+void printPointer(int i) {
+	int* iptr = &i;
+	// the unary * operator dereferences the pointer
+	std::cout << "i = " << *iptr << "\n";
+	std::cout << " & i = " << iptr << "\n";
+}
+
 int main() {
 
 
