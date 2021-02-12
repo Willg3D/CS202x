f3a186a41b9d754009e0deee1bd6d25ed6f32789
William Glass <willg.glass94@gmail.com>
2021-02-12 04:39:37 -0900

    Test to check is text is lower, bug with 0 not terminiting exist still.

--- HW2_3_LowerCaseLetters/main.cpp
+++ HW2_3_LowerCaseLetters/main.cpp
@@ -2,9 +2,9 @@
 
 void to_lower(char* s) {
 	for (size_t i = 0; s[i] != '0'; i++){
-		//65 A to 90 Z
+		//65 A to 90 Z and 32 defference to lowercase
 		if (s[i] >= 65 && s[i] <= 90) {
-			std::cout << s[i];
+			s[i] = s[i] + 32;
 		}
 	}
 }
@@ -12,6 +12,6 @@ void to_lower(char* s) {
 int main() {
 	char text[] = "Hello World0!!";
 	to_lower(text);
-
+	std::cout << text;
 	return 0;
 }
