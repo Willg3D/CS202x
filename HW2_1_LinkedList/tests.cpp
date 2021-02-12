21382576caad9136db3c280e8b89c193b10f870d
William Glass <willg.glass94@gmail.com>
2021-02-11 20:07:34 -0900

    Added a function to quickly fill struct with values.

--- HW2_1_LinkedList/tests.cpp
+++ HW2_1_LinkedList/tests.cpp
@@ -12,14 +12,37 @@ struct Monster { // Value Struct
 	int def;
 };
 
-// Queue
-TEST_CASE("Push and Pop Test", "[Queue]") {
+void enterMonValue(Monster& name, int id, char type, int hp, int sta, int att, int def) {
+	name.id = id;
+	name.type = type;
+	name.hp = hp;
+	name.sta = sta;
+	name.att = att;
+	name.def = def;
+}
 
+// Queue
+TEST_CASE("Push and Pop Test Queue", "[Queue]") {
+	std::list<Monster> test1;
+	Monster v1,v2,v3,v4;
+	enterMonValue(v1, 1, 'W', 1, 1, 1, 1);
+	enterMonValue(v2, 2, 'W', 2, 2, 2, 2);
+	enterMonValue(v3, 3, 'W', 3, 3, 3, 3);
+	enterMonValue(v4, 4, 'W', 4, 4, 4, 4);
 
 }
 // Stack
+TEST_CASE("Push and Pop Test Stack", "[Stack]") {
 
+
+}
 // Insert and Find
+TEST_CASE("Insert and Find", "") {
 
+
+}
 // Print the list out
+TEST_CASE("Print the list out", "") {
 
+
+}
