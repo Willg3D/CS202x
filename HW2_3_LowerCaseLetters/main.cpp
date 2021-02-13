61298b05754b388fc15072a51c4e7018a32ef4a2
William Glass <willg.glass94@gmail.com>
2021-02-12 19:12:03 -0900

    Fixed so terminating zero no longer needed to be typed in char array.

--- HW2_3_LowerCaseLetters/main.cpp
+++ HW2_3_LowerCaseLetters/main.cpp
@@ -1,5 +1,8 @@
+// William Glass
+// CS 202x
+// HW 2_3
 void to_lower(char* s) {
-	for (size_t i = 0; s[i] != '0'; i++){
+	for (size_t i = 0; s[i] != 0; i++){
 		//65 A to 90 Z and 32 defference to lowercase (ascii)
 		if (s[i] >= 65 && s[i] <= 90) {
 			s[i] = s[i] + 32;
@@ -8,12 +11,11 @@ void to_lower(char* s) {
 }
 
 int main() {
-	// text array with terminating zero
-	char text[] = "Hello, World!0";
+	char text[] = "Hello, World!";
 	to_lower(text);
 
 	// a check without using iostream header
-	for (size_t i = 0; text[i] != '0'; i++) {
+	for (size_t i = 0; text[i] != 0; i++) {
 		if (text[i] >= 65 && text[i] <= 90) {
 			// If check sees ascii values for upper case
 			// letters appear then the program will crash.
