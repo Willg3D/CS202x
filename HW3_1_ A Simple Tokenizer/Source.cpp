0cd454afcad01cbe0e21d056dbbc72d1a79e4966
William Glass <willg.glass94@gmail.com>
2021-02-24 23:24:42 -0900

    Read assignment tweak program

--- HW3_1_ A Simple Tokenizer/Source.cpp
+++ HW3_1_ A Simple Tokenizer/Source.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
+using std::cout;
 #include <string>
+using std::string;
+#include <fstream>
+#include <sstream> //istreamstring used for words
 #include <vector>
 
 std::vector<std::string> stringW_to_Vector(const std::string &stringW) {
@@ -19,21 +23,30 @@ std::vector<std::string> stringW_to_Vector(const std::string &stringW) {
 	return vector;
 }
 
-int main() {
-	std::string tokens;
-	std::getline(std::cin, tokens);
-	std::vector<std::string> tokens_vector = stringW_to_Vector(tokens);
-
-
-	//int numTokens = std::strlen(tokens.c_str());
-
-
-
-	for (size_t i = 0; i < tokens_vector.size(); i++)
-	{
-		std::cout << tokens_vector[i] << "\n";
+void readFile_vector(string filename, std::vector<string>& book) {
+	std::ifstream open(filename);
+	string word;
+	if (!open) { // quick error check
+		cout << "Error Can't Load Data!" << std::endl;
 	}
-	//	//std::string temp = 
+	else {
+		while (!open.eof()) {
+			string data;
+			std::getline(open, data);
+			std::istringstream iss(data);
+
+			while (iss >> word) {
+				book.push_back(word);
+			}
+		}
+	}
+}
+
+int main() {
+	//std::string tokens;
+	//std::getline(std::cin, tokens);
+	//std::vector<std::string> tokens_vector = stringW_to_Vector(tokens);
+
 
 
 	return 0;
