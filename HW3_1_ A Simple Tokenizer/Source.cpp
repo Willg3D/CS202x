e800bc062a93f55512fc8f3be0a8eaff8df17a05
William Glass <willg.glass94@gmail.com>
2021-02-24 23:13:15 -0900

    Made string words to vector its own function

--- HW3_1_ A Simple Tokenizer/Source.cpp
+++ HW3_1_ A Simple Tokenizer/Source.cpp
@@ -2,24 +2,31 @@
 #include <string>
 #include <vector>
 
-int main() {
-	std::string tokens;
-	std::vector<std::string> tokens_vector;
-	std::getline(std::cin, tokens);
-
-	int numTokens = std::strlen(tokens.c_str());
+std::vector<std::string> stringW_to_Vector(const std::string &stringW) {
 	std::string temp;
+	std::vector<std::string> vector;
 	int i = 0;
-	for (size_t x = 0; x <= tokens.size(); x++){
-		if (tokens[x] != ' '){
-			temp.push_back(tokens[x]);
+	for (size_t x = 0; x <= stringW.size(); x++) {
+		if (stringW[x] != ' ') {
+			temp.push_back(stringW[x]);
 		}
-		if (tokens[x] == ' ' || x == tokens.size()){
-			tokens_vector.push_back(temp);
+		if (stringW[x] == ' ' || x == stringW.size()) {
+			vector.push_back(temp);
 			temp.clear();
 			i++;
 		}
 	}
+	return vector;
+}
+
+int main() {
+	std::string tokens;
+	std::getline(std::cin, tokens);
+	std::vector<std::string> tokens_vector = stringW_to_Vector(tokens);
+
+
+	//int numTokens = std::strlen(tokens.c_str());
+
 
 
 	for (size_t i = 0; i < tokens_vector.size(); i++)
